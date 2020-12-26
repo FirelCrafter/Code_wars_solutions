284e3ac369a8728fa86541f9925c635a7b8d2836
Mikhail <firecrafter62@gmail.com>
2020-12-26 16:04:33 +0200

    5_kyu+1

---
+++ 5_kyu/Extract_the_domain_name_from_url.py
@@ -0,0 +1,10 @@
+from re import compile, match
+
+REGEX = compile(r'(http[s]*://)?(www.)?(?P<domain>[\w-]+)\.')
+
+
+def domain_name(url):
+    return match(REGEX, url).group('domain')
+
+
+print(domain_name("http://google.com"))
