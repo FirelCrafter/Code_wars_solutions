acf89989550ef7f9f616fda89be2e9ea1f6ac9a9
Mikhail <firecrafter62@gmail.com>
2020-12-29 16:39:40 +0200

    7_kyu+1

--- 7_kyu/highest_and_lowest.py
+++ 7_kyu/highest_and_lowest.py
@@ -1,6 +1,6 @@
 def high_and_low(numbers):
     n = sorted([int(num) for num in numbers.split()])
-    return ' '.join([str(n[-1]), str(n[0])]).strip(' ')
+    return '{} {}'.format(n[-1], n[0])
 
 
 print(high_and_low('4 5 29 54 4 0 -214 542 -64 1 -3 6 -6'))
